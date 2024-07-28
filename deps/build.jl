5b3f22bc29de43eadc0a46e339d0540bdfa77eb9
brainvs <vasfi@vasfi.me>
2024-07-28 08:28:02 +0300

    Dependency build file adding log output

--- deps/build.jl
+++ deps/build.jl
@@ -1,15 +1,21 @@
-using Pkg
+import Pkg
+
+println("Running build.jl")
 
 executables = [
-    joinpath(@__DIR__, "src", "DataCentricKMeans_linux.out"),
-    joinpath(@__DIR__, "src", "DataCentricKMeans_universal.out"),
-    joinpath(@__DIR__, "src", "DataCentricKMeans_windows.exe")
+    joinpath(@__DIR__, "..", "src", "DataCentricKMeans_linux.out"),
+    joinpath(@__DIR__, "..", "src", "DataCentricKMeans_universal.out"),
+    joinpath(@__DIR__, "..", "src", "DataCentricKMeans_windows.exe")
 ]
 
 for exe in executables
     if isfile(exe)
         println("Setting executable permissions for $exe")
         run(`chmod +x $exe`)
+    else
+        println("Executable $exe not found")
     end
 end
+
+println("Finished build.jl")
 
